406ddc0e2d0286b3a52ebecabfc1024ff75b7f2a
MatiasColomba <96466888+MatiasColomba@users.noreply.github.com>
2021-12-21 09:51:59 +0100

    Add files via upload

---
+++ freeSeats.js
@@ -0,0 +1,12 @@
+freeSeats = [15,20,25,30];
+desiredSeat = 50
+
+function availableSeats(){
+        if(freeSeats.indexOf(desiredSeat)>=0){
+            console.log("Congratulations, seat number " + desiredSeat + " is available")
+        }else{
+            console.log("Sorry, seat number "+ desiredSeat +  " is busy, but there are still " + freeSeats.length+ " available seats")
+        }
+    
+}
+console.log(availableSeats())
